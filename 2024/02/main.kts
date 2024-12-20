8db596b8a9d8b5901e6faf68624906ae1222c759
Paulus van Sluis <paulus.van.sluis@persgroep.net>
2024-12-20 13:50:54 +0100

    2 part 2

--- 2024/02/main.kts
+++ 2024/02/main.kts
@@ -1,6 +1,8 @@
 import java.io.File
 import kotlin.math.abs
 
+typealias Line = List<Int>
+
 val fileName = "realData.txt"
 //val fileName = "testData.txt"
 val inputStream = File(fileName).inputStream()
@@ -13,24 +15,23 @@ val parsed = inputString.split("\n").map { line ->
 fun <T> List<T>.allAdjacent(predicate: (Pair<T, T>) -> Boolean): Boolean =
     this.windowed(2).map { Pair(it[0], it[1]) }.all(predicate)
 
-fun List<Int>.isIncreasing() = allAdjacent { it.first < it.second }
+fun Line.isIncreasing() = allAdjacent { it.first < it.second }
 
-fun List<Int>.isDecreasing() = allAdjacent { it.first > it.second }
+fun Line.isDecreasing() = allAdjacent { it.first > it.second }
 
-fun List<Int>.differsByAtLeastOneAndAtMostThree() = allAdjacent {
+fun Line.differsByAtLeastOneAndAtMostThree() = allAdjacent {
     abs(it.first - it.second) in 1..3
 }
 
-fun List<Int>.isSafe() = (isIncreasing() || isDecreasing()) && differsByAtLeastOneAndAtMostThree()
+fun Line.isSafe() = (isIncreasing() || isDecreasing()) && differsByAtLeastOneAndAtMostThree()
 
 val safeLevels = parsed.count { it.isSafe() }
 println("safe levels: $safeLevels")
 
 // part 2
 
-val List<Int>.subLists: List<List<Int>>
+val Line.subLists: List<Line>
     get() = List(size) { i -> this.filterIndexed { index, _ -> index != i } }
-
 
 val safeLevelsWithProblemDampener = parsed.count { line -> line.subLists.any { it.isSafe() } }
 
