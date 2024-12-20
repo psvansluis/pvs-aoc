b7478dcaa1cd69e0f41b20f8a42f7e69b05211df
Paulus van Sluis <paulus.van.sluis@persgroep.net>
2024-12-20 15:46:59 +0100

    03 part 2

--- 2024/03/main.kts
+++ 2024/03/main.kts
@@ -1,13 +1,26 @@
 import java.io.File
 
 val fileName = "realData.txt"
-//val fileName = "testData.txt"
+//val fileName = "testData2.txt"
 val inputStream = File(fileName).inputStream()
 val inputString = inputStream.bufferedReader().readText()
 
-val regex = Regex("mul\\((\\d+),(\\d+)\\)")
-val muls: List<List<Int>> =
-    regex.findAll(inputString).toList().map { match -> match.groups.drop(1).map { it!!.value.toInt() } }
+val regexMul = Regex("mul\\((\\d+),(\\d+)\\)")
+
+fun getMuls(input: String): Sequence<List<Int>> =
+    regexMul.findAll(input).map { match -> match.groups.drop(1).map { it!!.value.toInt() } }
+
+val muls: Sequence<List<Int>> = getMuls(inputString)
 
 val sumOfProducts = muls.sumOf { it[0] * it[1] }
 println("sumOfProducts: $sumOfProducts")
+
+// part 2
+val doParts: String =
+    ("do()$inputString")
+        .split("don't()")
+        .flatMap { it.split("do()").drop(1) }
+        .joinToString("")
+
+val sumOfEnabledProducts = getMuls(doParts).sumOf { it[0] * it[1] }
+println("sumOfEnabledProducts: $sumOfEnabledProducts")
