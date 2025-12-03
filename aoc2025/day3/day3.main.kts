5e63a5ee0df853836e6a29fd5f9d3a234257fd4e
Paulus van Sluis <paulus.van.sluis@persgroep.net>
2025-12-03 12:59:50 +0100

    day3 part2 cleanup

--- aoc2025/day3/day3.main.kts
+++ aoc2025/day3/day3.main.kts
@@ -2,7 +2,7 @@
 
 import java.io.File
 
-val mode = "example"
+val mode = "input"
 
 val lines = File("$mode.txt").readLines()
 
@@ -10,23 +10,15 @@ typealias Bank = List<Int>
 
 val parsedLines: List<Bank> = lines.map { line -> line.chunked(1).map(String::toInt) }
 
-fun Bank.getLargestJoltagePart1(): Int {
-    val firstDigit = this.dropLast(1).max()
-    val secondDigit = this.dropWhile { it != firstDigit }.drop(1).max()
-    return (firstDigit * 10) + secondDigit
-}
-
-parsedLines.sumOf(Bank::getLargestJoltagePart1).let(::println)
-
-fun <I, O> memoize(memo: MutableMap<I, O>, f: (I) -> O): (I) -> O = { input -> memo.getOrPut(input) { f(input) } }
-
-val memo = mutableMapOf<Bank, Long>()
-
-tailrec fun Bank.getLargestJoltagePart2(acc: Long = 0L, length: Int): Long {
+tailrec fun Bank.getLargestJoltage(acc: Long = 0L, length: Int): Long {
     if (length == 0) return acc
     val digitToAdd = this.dropLast(length - 1).max()
     val rest = this.dropWhile { it != digitToAdd }.drop(1)
-    return rest.getLargestJoltagePart2((acc * 10) + digitToAdd, length - 1)
+    return rest.getLargestJoltage((acc * 10) + digitToAdd, length - 1)
 }
 
-parsedLines.map { line -> line.getLargestJoltagePart2(length =  12) }.sum().let(::println)
+print("part 1: ")
+parsedLines.sumOf { bank -> bank.getLargestJoltage(length = 2) }.let(::println)
+
+print("part 2: ")
+parsedLines.sumOf { bank -> bank.getLargestJoltage(length = 12) }.let(::println)
